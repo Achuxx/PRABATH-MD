4cadb8d4389c88d239435f34ab7ba8fe7d2aa51e
Achuxx <166356043+Achuxx@users.noreply.github.com>
2024-04-14 16:38:19 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -3,8 +3,8 @@
 ░█▄▄█ ░█▄▄▀ ░█▄▄█ ░█▀▀▄ ░█▄▄█ ─░█── ░█▀▀█ ▀▀ ░█░█░█ ░█─░█ 
 ░█─── ░█─░█ ░█─░█ ░█▄▄█ ░█─░█ ─░█── ░█─░█ ── ░█──░█ ░█▄▄▀
 
-* @project_name PRABATH-MD
-* @authors Prabath Kumara & sachibot-team(Sachintha) <https://github.com/prabathLK>
+* @project_name VIBINESH-MD
+* @authors VIBINESH <https://github.com/prabathLK>
 * @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
 * @link <https://github.com/prabathLK/PRABATH-MD>
 * @version 2.0.6
